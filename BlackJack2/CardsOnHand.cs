39326100bb440e555027587949981812850609d3
Noa Norden <noa.norden@gmail.com>
2025-01-29 22:38:59 +0100

    Added c# project

---
+++ BlackJack2/CardsOnHand.cs
@@ -0,0 +1,29 @@
+public class CardsOnHand 
+{
+    public List<int> LstCards = new List<int>();
+    public int score { get; set; }
+
+    static Random rnd = new Random();
+    public CardsOnHand()
+    {
+        int c1 = rnd.Next(2, 12);
+        int c2 = rnd.Next(2, 12);
+
+        LstCards.Add(c1);
+        LstCards.Add(c2);
+    }
+
+    public void DrawCard()
+    {
+        int newCard = rnd.Next(2, 12);
+        LstCards.Add(newCard);
+    }
+
+    public int CalcScore()
+    {
+        return LstCards.Sum();
+    }
+
+    
+    
+}
---
+++ BlackJack2/Program.cs
@@ -0,0 +1,9 @@
+﻿CardsOnHand coh1 = new CardsOnHand();
+
+Console.WriteLine("Your hand is:");
+
+coh1.DrawCard();
+
+foreach (int c in coh1.LstCards) { Console.WriteLine(c); }
+
+Console.WriteLine("Your score: " + coh1.CalcScore());
